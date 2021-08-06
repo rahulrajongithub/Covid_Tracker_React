cea0872b8e4352b93ee1dfc2516ae00f66699259
Rahul Raj <themailtorahul@gmail.com>
2021-08-06 11:59:24 +0530

    Added a drop down. Starting on UseEffect

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,9 +1,56 @@
-import './App.css';
+import React, {useEffect, useState} from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState(['USA', 'UK', 'INDIA']);
+  // State = How to write variable in react
+
+  //https://disease.sh/v3/covid-19/countries
+
+  // USEEFFECT = Runs a piece of code
+  // based on a given condition
+
+  useEffect(() => {
+    // The code inside here will run once when the component
+    // loads and not again
+  }, [countries]);
+  
   return (
     <div className="app">
-      <h1>Let's Create the Covid Tracker Application</h1>
+      <div className="app__header">
+        <h1>C-TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop through all the countries 
+            and show a drop down list of the 
+            options */}
+
+            {
+              countries.map(country => (
+                <MenuItem value={country}>{country}</MenuItem>
+              ))
+            }
+
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">qwert</MenuItem>
+            <MenuItem value="worldwide">dfg</MenuItem>
+            <MenuItem value="worldwide">Worlnvbnvbndwode</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* Header */}
+      {/* Title + Select Input Dropdown*/}
+
+      {/* Infobox1 */}
+      {/* Infobox2 */}
+      {/* Infobox3 */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
